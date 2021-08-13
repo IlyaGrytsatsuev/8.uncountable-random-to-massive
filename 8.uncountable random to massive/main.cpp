cd3c89635083b8daad357252991771647911f8bc
IlyaGrytsatsuev <ilya.gr2003@yandex.ru>
2021-08-13 21:37:50 +0300

    Initial Commit

---
+++ 8.uncountable random to massive/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  8.uncountable random to massive
+//
+//  Created by Илья Грицацуев on 13.08.2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
